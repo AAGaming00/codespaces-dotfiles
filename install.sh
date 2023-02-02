70c54eb50185d6113c028eb43c8b977d54e1742c
AAGaming <aa@mail.catvibers.me>
2023-02-01 20:37:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+# Kill the bash shell that automatically opened
+pgrep -f "/usr/bin/bash --init-file" | xargs kill -SIGINT
+# Clone dotfiles from Gitea
 curl -fsLS git.io/chezmoi | sh -s -- init --apply https://git.catvibers.me/aa/dotfiles
+# Begin dotfiles setup
+zsh .zsh/.zshrc
